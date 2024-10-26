c61368e329d474ca00d2e5e8625e4b501888edfe
Karina Rozenboym <karinarozik1@gmail.com>
2024-10-27 02:37:56 +0300

    Добавьте файлы проекта.

---
+++ Collatz-Conjecture/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public static class CollatzConjecture
+{
+    public static int Steps(int n)
+    {
+        if (n < 1)
+        {
+            throw new ArgumentOutOfRangeException(nameof(n), "Input must be a positive integer.");
+        }
+
+        int steps = 0;
+
+        while (n != 1)
+        {
+            if (n % 2 == 0)
+            {
+                n /= 2;
+            }
+            else
+            {
+                n = 3 * n + 1;
+            }
+            steps++;
+        }
+
+        return steps;
+    }
+}
